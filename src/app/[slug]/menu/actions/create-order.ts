07ca883a05f6032baf2bb40a0a630adf69912452
luciocarvalho10 <luciocarvalho10@hotmail.com.br>
2025-03-03 07:24:03 -0400

    feat: ensure cache is revalidate when creating an order

--- src/app/[slug]/menu/actions/create-order.ts
+++ src/app/[slug]/menu/actions/create-order.ts
@@ -1,6 +1,7 @@
-"use server"
+"use server";
 
 import { Order } from "@prisma/client";
+import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
 import { db } from "@/lib/prisma";
@@ -8,45 +9,51 @@ import { db } from "@/lib/prisma";
 import { ICartProduct } from "../contexts/cart";
 import { removeCpfPunctuation } from "../helpers/cpf";
 
-
-interface ICreateOrderInput extends Pick<Order, "consumptionMethod" | "customerCpf" | "customerName">{
-    products: Pick<ICartProduct, "id" | "quantity">[]
-    slug: string
+interface ICreateOrderInput
+  extends Pick<Order, "consumptionMethod" | "customerCpf" | "customerName"> {
+  products: Pick<ICartProduct, "id" | "quantity">[];
+  slug: string;
 }
 
 export const createOrder = async (input: ICreateOrderInput) => {
-    const restaurant = await db.restaurant.findUnique({
-        where: {
-            slug: input.slug
-        }
-    })
-    if (!restaurant) throw new Error("Restaurant not found")
-    const productWithPrices = await db.product.findMany({
-        where: {
-            id: {
-                in: input.products.map(product => product.id)
-            }
-        }
-    })
-    const productsWithPricesAndQuantities = input.products.map(product => ({
-        productId: product.id,
-        quantity: product.quantity,
-        price: productWithPrices.find(p => p.id === product.id)!.price
-    }))
-    await db.order.create({
-        data: {
-            consumptionMethod: input.consumptionMethod,
-            customerCpf: removeCpfPunctuation(input.customerCpf),
-            customerName: input.customerName,
-            status: "PENDING",
-            restaurantId: restaurant.id,
-            orderProducts: {
-                createMany: {
-                    data: productsWithPricesAndQuantities
-                }
-            },
-            total: productsWithPricesAndQuantities.reduce((sum, product) => sum + product.price * product.quantity, 0)
-        }
-    })
-    redirect(`/${input.slug}/orders?cpf=${removeCpfPunctuation(input.customerCpf)}`)
-}
+  const restaurant = await db.restaurant.findUnique({
+    where: {
+      slug: input.slug,
+    },
+  });
+  if (!restaurant) throw new Error("Restaurant not found");
+  const productWithPrices = await db.product.findMany({
+    where: {
+      id: {
+        in: input.products.map((product) => product.id),
+      },
+    },
+  });
+  const productsWithPricesAndQuantities = input.products.map((product) => ({
+    productId: product.id,
+    quantity: product.quantity,
+    price: productWithPrices.find((p) => p.id === product.id)!.price,
+  }));
+  await db.order.create({
+    data: {
+      consumptionMethod: input.consumptionMethod,
+      customerCpf: removeCpfPunctuation(input.customerCpf),
+      customerName: input.customerName,
+      status: "PENDING",
+      restaurantId: restaurant.id,
+      orderProducts: {
+        createMany: {
+          data: productsWithPricesAndQuantities,
+        },
+      },
+      total: productsWithPricesAndQuantities.reduce(
+        (sum, product) => sum + product.price * product.quantity,
+        0,
+      ),
+    },
+  });
+  revalidatePath(`/${input.slug}/orders`);
+  redirect(
+    `/${input.slug}/orders?cpf=${removeCpfPunctuation(input.customerCpf)}`,
+  );
+};
